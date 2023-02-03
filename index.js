33e596b2ecffd901ce7cfd0be79a67b86423bd4a
Rachalle Way <way.rachalle@gmail.com>
2023-02-03 14:20:36 -0500

    solved

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+var questionsArr = [
+    {question: 'The sky is blue',
+     answer: true},
+    {question: 'Grass is green',
+    answer: true},
+    {question: 'The Beatles are from Italy',
+     answer: false},
+    {question: 'CSS stands for cascading stylish shrimp',
+     answer: false},
+    {question: 'Kermit the Frog is a frog',
+     answer: true}    
+]
+
+var numCorrect=0
+
+function runQuiz (){
+    for (var i=0; i<questionsArr.length; i++){
+        if(confirm(questionsArr[i].question)===questionsArr[i].answer){
+            numCorrect++
+        }
+    }
+    var quizScore = Math.round ((numCorrect/questionsArr.length)*100)
+
+    alert ("You scored" + quizScore + "%")
+    numCorrect=0
+}
